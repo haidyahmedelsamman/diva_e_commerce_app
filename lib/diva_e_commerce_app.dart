02200dc2a4854683c8be695a340e03aa33753633
Haidy <haidyahmedelsamman@gmail.com>
2024-09-25 23:48:33 +0300

    Create Project Sturcture +add client file +add dependencies +add main sturcture folders

---
+++ lib/diva_e_commerce_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DivaECommerceApp extends StatelessWidget {
+  const DivaECommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Diva E-Commerce App'),
+      ),
+    );
+  }
+}
