4de108e65c4404c3bec754a0718f334134e451ae
s4r4h4mdy0x01 <sara101hamdy@gmail.com>
2024-10-01 02:04:37 +0300

    Add Sign Up text Widget

---
+++ lib/features/sign_up/ui/sign_up_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../core/constants/string_manager.dart';
+import '../../../core/helpers/spacing.dart';
+import '../../../core/theme/text_style_manager.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 30.h, horizontal: 30.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              verticalSpace(100),
+              Text(
+                StringManager.signUp,
+                style: TextStyleManager.font24BlackRegular,
+              ),
+              verticalSpace(16),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
