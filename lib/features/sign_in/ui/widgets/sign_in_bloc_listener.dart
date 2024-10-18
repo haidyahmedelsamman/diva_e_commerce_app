5912aa61c6082dbf75b4427fce96c5d0ab1bed65
s4r4h4mdy0x01 <sara101hamdy@gmail.com>
2024-10-18 12:27:06 +0300

    Add Logic to Ui Sign In screen  🚑

--- lib/features/sign_in/ui/widgets/sign_in_bloc_listener.dart
+++ lib/features/sign_in/ui/widgets/sign_in_bloc_listener.dart
@@ -1,19 +1,19 @@
 import 'package:diva_e_commerce_app/core/extensions/build_context_extensions.dart';
-import 'package:diva_e_commerce_app/core/theme/colors_manager.dart';
-import 'package:diva_e_commerce_app/core/theme/text_style_manager.dart';
+import 'package:diva_e_commerce_app/features/sign_in/logic/sign_in_cubit.dart';
+import 'package:diva_e_commerce_app/features/sign_in/logic/sign_in_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/routes/app_router.dart';
-import '../../logic/sign_up_cubit.dart';
-import '../../logic/sign_up_state.dart';
+import '../../../../core/theme/colors_manager.dart';
+import '../../../../core/theme/text_style_manager.dart';
 
-class SignupBlocListener extends StatelessWidget {
-  const SignupBlocListener({super.key});
+class SignInBlocListener extends StatelessWidget {
+  const SignInBlocListener({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<SignUpCubit, SignUpState>(
+    return BlocListener<SignInCubit, SignInState>(
       listenWhen: (previous, current) =>
           current is Loading || current is Success || current is Error,
       listener: (context, state) {
@@ -28,9 +28,9 @@ class SignupBlocListener extends StatelessWidget {
               ),
             );
           },
-          success: (signupResponse) {
+          success: (loginResponse) {
             context.pop();
-            showSuccessDialog(context);
+            context.pushNamed(AppRoutes.homeScreenRoute);
           },
           error: (error) {
             setupErrorState(context, error);
@@ -38,38 +38,6 @@ class SignupBlocListener extends StatelessWidget {
         );
       },
       child: const SizedBox.shrink(),
-    );
-  }
-
-  void showSuccessDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Signup Successful'),
-          content: const SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text('Congratulations, you have signed up successfully!'),
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: ColorsManager.primary,
-                disabledForegroundColor: Colors.grey.withOpacity(0.38),
-              ),
-              onPressed: () {
-                // I will let him go to Screen Login, but after merging the brunchs
-                context.pushNamed(AppRoutes.signInScreenRoute);
-              },
-              child: const Text('Continue'),
-            ),
-          ],
-        );
-      },
     );
   }
 
